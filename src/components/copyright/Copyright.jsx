7251a27b355384400e6858c6c301a4cfa2980549
justin0x7 <imasnpsorrels5@gmail.com>
2023-12-12 09:57:10 +0900

    first commit

---
+++ src/components/copyright/Copyright.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./copyright.css";
+
+function Copyright() {
+  return (
+    <div className="cr">
+      <p>© Copyright Agency and contributors all rights reserved 2024</p>
+    </div>
+  );
+}
+
+export default Copyright;
---
+++ src/container/github/Github.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./github.css";
+import { FaInstagram, FaGithub } from "react-icons/fa";
+import profile from "../../assets/daniel.png";
+
+function Github() {
+  return (
+    <div className="github" id="github">
+      <div className="github-header">
+        <div className="github-header-title">
+          <h1>Happy Man</h1>
+        </div>
+        <div className="github-bar-container">
+          <div className="github-bar"></div>
+        </div>
+      </div>
+      <div className="github-content section-padding">
+        <div className="github-content-pic">
+          <img src={profile} alt="github-icon-gradient" />
+        </div>
+
+        <div className="github-content-links">
+          <h3 className="gradient-text">Good friend</h3>
+          <p>
+            Happy Day To Your Family
+          </p>
+          {/* <div className="github-content-insta">
+            <FaInstagram size={25} />
+            <p>
+              <a href="https://www.instagram.com/unk.iman.dev/">
+                Follow me on Instagram
+              </a>
+            </p>
+          </div> */}
+          {/* <div className="github-content-insta">
+            <FaGithub size={25} />
+            <p>
+              <a href="https://github.com/Im-unk">
+                Check out more projects on Github!
+              </a>{" "}
+            </p>
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Github;
